4bf2970b70b281b16f47b3c208cc8b83d840512e
Adam Szeptycki <adamszeptycki@gmail.com>
2019-01-05 19:06:37 +0100

    add typescript and load images and stuff

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e2b12326a06a8f08548f";
+/******/ 	var hotCurrentHash = "317593fca8cb3923c533";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 const dev = process.env.NODE_ENV !== 'production'
-module.exports = {
-    assetPrefix: dev ? undefined : 'production'
-}
+
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : !process.env.NOW_REGION
+      ? require('next/constants')
+      : require('next-server/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {
+        assetPrefix: 'production',
+    };
+  }
+
+  // ✅ Put the require call here.
+const withCSS = require('@zeit/next-css')
+const withTypescript = require('@zeit/next-typescript')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const withImages = require('next-images')
+
+return withCSS(
+    withImages(
+    withTypescript({
+    assetPrefix: dev ? undefined : 'production',
+    webpack(config, options) {
+        // Do not run type checking twice:
+        if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
+        return config
+   }
+ }))
+)
+};
